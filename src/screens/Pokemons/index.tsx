2440a8ff7067a752409758e40fe3090fc4b70c05
vitorserrano <vitorcirinoserrano@gmail.com>
2021-12-17 01:21:41 -0300

    feat: create pokemons screen

--- src/screens/Pokemons/index.tsx
+++ src/screens/Pokemons/index.tsx
@@ -1,14 +1,37 @@
 import React, { useState, useEffect, useCallback } from 'react'
-import { View, Text, Image, FlatList } from 'react-native'
+import {
+  View,
+  Text,
+  Image,
+  FlatList,
+  TouchableOpacity,
+  ActivityIndicator,
+} from 'react-native'
 
-import { getPokemons, PokemonsResponse } from '../../services/pokemons'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { useNavigation } from '@react-navigation/native'
+
+import { getPokemons, PokemonResponse } from '../../services/pokemons'
+import { getFullOrderNumber, getColorByType } from '../../utils'
+
 import styles from './styles'
 
 export const Pokemons = () => {
-  const [pokemons, setPokemons] = useState<PokemonsResponse[]>([])
+  const [pokemons, setPokemons] = useState<PokemonResponse[]>([])
   const [offset, setOffset] = useState(0)
 
+  const navigation = useNavigation()
+
   const loadPokemons = async () => {
+    try {
+      const response = await getPokemons()
+      setPokemons(response)
+    } catch (error) {
+      console.error('Failed to load pokemons', error)
+    }
+  }
+
+  const loadMorePokemons = async () => {
     try {
       const limit = 20
       const response = await getPokemons(offset, limit)
@@ -16,39 +39,68 @@ export const Pokemons = () => {
       setPokemons(oldValue => [...oldValue, ...response])
       setOffset(oldValue => oldValue + limit)
     } catch (error) {
-      console.log(error)
+      console.error('Failed to load more pokemons', error)
     }
   }
-
-  const keyExtractor = useCallback(
-    (item: PokemonsResponse) => String(item.order),
-    []
-  )
-
-  const renderItem = useCallback(
-    ({ item }: { item: PokemonsResponse }) => (
-      <View key={item.order}>
-        <Text>{item.name}</Text>
-        <Image style={styles.thumbnail} source={{ uri: item.thumbnail }} />
-      </View>
-    ),
-    []
-  )
 
   useEffect(() => {
     loadPokemons()
   }, [])
 
+  const navigateToPokemonDetails = (pokemon: PokemonResponse) =>
+    navigation.navigate('PokemonDetails', { pokemon })
+
+  const keyExtractor = useCallback(
+    (item: PokemonResponse) => String(item.order),
+    []
+  )
+
+  const renderItem = useCallback(
+    ({ item }: { item: PokemonResponse }) => (
+      <TouchableOpacity
+        key={item.order}
+        style={{
+          ...styles.card,
+          backgroundColor: getColorByType(item.types[0].type.name),
+        }}
+        activeOpacity={0.8}
+        onPress={() => navigateToPokemonDetails(item)}
+      >
+        <View>
+          <Text style={styles.order}>{getFullOrderNumber(item.id)}</Text>
+          <Text style={styles.name}>{item.name}</Text>
+        </View>
+        <Image style={styles.thumbnail} source={{ uri: item.thumbnail }} />
+      </TouchableOpacity>
+    ),
+    [navigateToPokemonDetails]
+  )
+
+  const renderFooter = useCallback(
+    () => (
+      <View style={styles.loading}>
+        <ActivityIndicator color="black" size="large" />
+      </View>
+    ),
+    []
+  )
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.title}>Pokeapp</Text>
+      </View>
+
       <FlatList
         data={pokemons}
         keyExtractor={keyExtractor}
         renderItem={renderItem}
-        onEndReached={loadPokemons}
+        ListFooterComponent={renderFooter}
+        onEndReached={loadMorePokemons}
         onEndReachedThreshold={0.1}
         numColumns={2}
+        showsHorizontalScrollIndicator={false}
       />
-    </View>
+    </SafeAreaView>
   )
 }
--- src/screens/Pokemons/styles.ts
+++ src/screens/Pokemons/styles.ts
@@ -4,14 +4,44 @@ import { theme } from '../../styles/theme';
 
 export default StyleSheet.create({
   container: {
-    padding: 60,
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    paddingLeft: 12,
     backgroundColor: theme.colors.background,
   },
+  header: {
+    marginBottom: 24,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: theme.colors.text,
+  },
+  card: {
+    flex: 1,
+    padding: 14,
+    borderRadius: 20,
+    marginBottom: 12,
+    marginRight: 12,
+  },
+  order: {
+    fontSize: 14,
+    fontWeight: "bold",
+    color: theme.colors.text,
+  },
+  name: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textTransform: "capitalize",
+    color: theme.colors.background,
+  },
   thumbnail: {
-    width: 100,
-    height: 100,
+    width: 110,
+    height: 110,
+    marginTop: 12,
+    alignSelf: "flex-end",
+  },
+  loading: {
+    paddingVertical: 20,
   }
 })
