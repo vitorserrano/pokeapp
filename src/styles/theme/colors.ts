bba83e304a9caf3ba16ac1ddb5929e5ba8450c68
vitorserrano <vitorcirinoserrano@gmail.com>
2021-12-17 01:20:43 -0300

    feat: create theme

--- src/styles/theme/colors.ts
+++ src/styles/theme/colors.ts
@@ -1,3 +1,24 @@
 export const colors = {
   background: '#FFFFFF',
+  text: '#17171B',
+  types: {
+    bug: '#8CB230',
+    dark: '#58575F',
+    dragon: '#0F6AC0',
+    electric: '#EED535',
+    fairy: '#ED6EC7',
+    fighting: '#D04164',
+    fire: '#FFA756',
+    flying: '#748FC9',
+    ghost: '#556AAE',
+    grass: '#8BBE8A',
+    ground: '#DD7748',
+    ice: '#61CEC0',
+    normal: '#9DA0AA',
+    poison: '#A552CC',
+    psychic: '#EA5D60',
+    rock: '#BAAB82',
+    steel: '#417D9A',
+    water: '#4A90DA',
+  }
 }
